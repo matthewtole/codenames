5ab872659e21b997dbce79896b7ff882ff7a9f4c
Matthew Tole <mtole@pebble.com>
2017-02-08 19:24:16 -0800

    Replace "civilian" with "[innocent] bystander
    This moves the wording to match the official rules / documentation.
    
    Fixes #6

--- data/messages.js
+++ data/messages.js
@@ -2,7 +2,7 @@
 
 module.exports = {
   ASSASSIN: 'You picked the assassin!',
-  CIVILIAN: 'You picked a civilian!',
+  BYSTANDER: 'You picked an innocent bystander!',
   FRIENDLY_SPY: 'You picked correctly!',
   ENEMY_SPY: 'You picked the wrong one!',
   VICTORY: 'You found all of your spies!'
--- data/rules.js
+++ data/rules.js
@@ -5,21 +5,21 @@
 module.exports = {
   default: {
     ASSASSIN: 'The {% team %} team just lost the game.',
-    CIVILIAN: 'It is now the {% other_team %} team\'s turn.',
+    BYSTANDER: 'It is now the {% other_team %} team\'s turn.',
     FRIENDLY_SPY: 'Good job! Keep guessing!',
     ENEMY_SPY: 'Idiots! Your turn is now over!',
     VICTORY: 'Congratulations to the {% team %} team!'
   },
   drinking: {
     ASSASSIN: 'The {% team %} spymaster has to down their drink!',
-    CIVILIAN: 'The {% other_team %} spymaster gets to nominate a member of the {% team %} team to take a drink!',
+    BYSTANDER: 'The {% other_team %} spymaster gets to nominate a member of the {% team %} team to take a drink!',
     FRIENDLY_SPY: 'The {% team %} spymaster gets to nominate a member of the {% other_team %} team to take a drink!',
     ENEMY_SPY: 'Everyone on the {% team %} team has to take a drink!',
     VICTORY: 'Everyone on the {% other_team %} team has to take a drink!'
   },
   strip: {
     ASSASSIN: 'The {% team %} spymaster has to remove all their remaining clothes!',
-    CIVILIAN: 'The {% other_team %} spymaster gets to nominate a member of the {% team %} team to remove one item of clothing!',
+    BYSTANDER: 'The {% other_team %} spymaster gets to nominate a member of the {% team %} team to remove one item of clothing!',
     FRIENDLY_SPY: 'The {% team %} spymaster gets to nominate a member of the {% other_team %} team to remove one item of clothing!',
     ENEMY_SPY: 'Everyone on the {% team %} team has to remove one item of clothing!',
     VICTORY: 'Everyone on the {% other_team %} team has to remove one item of clothing!'
