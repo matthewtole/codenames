18763b84c5247f022f4b660951d44e711d3641ff
Matthew Tole <mtole@pebble.com>
2017-03-09 21:42:49 -0800

    Improve the admin panel

--- server.js
+++ server.js
@@ -8,6 +8,7 @@ const config = require('./config');
 const RoomManager = require('./lib/room-manager');
 const hbs = require('hbs');
 const hbsutils = require('hbs-utils')(hbs);
+const capitalize = require('capitalize');
 
 app.use(express.static('public'));
 app.use(express.static('dist'));
@@ -17,6 +18,7 @@ app.engine('html', hbs.__express); // eslint-disable-line no-underscore-dangle
 hbsutils.registerPartials(`${__dirname}/views/partials`);
 hbsutils.registerWatchedPartials(`${__dirname}/views/partials`);
 hbs.localsAsTemplateData(app);
+hbs.registerHelper('capitalize', capitalize);
 
 const roomManager = new RoomManager();
 roomManager.load();
