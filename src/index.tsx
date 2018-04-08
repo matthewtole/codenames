7566f0d2cae1800bed2ab6813ddc83516135de48
Matthew Tole <matthewtole@gmail.com>
2018-04-08 11:13:00 -0700

    Delete unused code

--- src/index.tsx
+++ src/index.tsx
@@ -9,20 +9,14 @@ import 'bulma-checkradio/bulma-checkradio.css';
 import './index.css';
 import { ConnectedRouter } from 'react-router-redux';
 import { Home } from './pages/Home';
-import { PageGame } from './pages/Game';
-import { GameSetupPage } from './pages/GameSetup';
 import { PageRoom } from './pages/Room';
-import { SetupPage } from './pages/Setup';
 
 ReactDOM.render(
   <Provider store={store}>
     <ConnectedRouter history={history}>
       <div className="Router">
         <Route exact={true} path="/" component={Home} />
-        <Route exact={true} path="/game/" component={GameSetupPage} />
-        <Route exact={true} path="/room/" component={SetupPage} />
         <Route exact={true} path="/room/:id/:mode?" component={PageRoom} />
-        <Route exact={true} path="/game/:id/:mode?" component={PageGame} />
       </div>
     </ConnectedRouter>
   </Provider>,
