874781e4d4279b4c472f11fca75e0c993d607aa3
Matthew Tole <matthewtole@gmail.com>
2018-04-11 16:27:35 -0700

    Fix the loading of environment variables

--- src/config.ts
+++ src/config.ts
@@ -5,21 +5,21 @@ import { FirebaseConfig } from './lib/firebase';
 dotenv.load();
 
 [
-  'FIREBASE_API_KEY',
-  'FIREBASE_AUTH_DOMAIN',
-  'FIREBASE_DATABASE_URL',
-  'FIREBASE_PROJECT_ID',
-  'FIREBASE_STORAGE_BUCKET',
-  'FIREBASE_MESSAGING_SENDER_ID',
+  'CN_FIREBASE_API_KEY',
+  'CN_FIREBASE_AUTH_DOMAIN',
+  'CN_FIREBASE_DATABASE_URL',
+  'CN_FIREBASE_PROJECT_ID',
+  'CN_FIREBASE_STORAGE_BUCKET',
+  'CN_FIREBASE_MESSAGING_SENDER_ID',
 ].forEach(key =>
   assert(process.env[key], `Missing required environment variable: "${key}"`)
 );
 
 export const firebase: FirebaseConfig = {
-  apiKey: process.env.FIREBASE_API_KEY!,
-  authDomain: process.env.FIREBASE_AUTH_DOMAIN!,
-  databaseURL: process.env.FIREBASE_DATABASE_URL!,
-  projectId: process.env.FIREBASE_PROJECT_ID!,
-  storageBucket: process.env.FIREBASE_STORAGE_BUCKET!,
-  messagingSenderId: process.env.FIREBASE_MESSAGING_SENDER_ID!,
+  apiKey: process.env.CN_FIREBASE_API_KEY!,
+  authDomain: process.env.CN_FIREBASE_AUTH_DOMAIN!,
+  databaseURL: process.env.CN_FIREBASE_DATABASE_URL!,
+  projectId: process.env.CN_FIREBASE_PROJECT_ID!,
+  storageBucket: process.env.CN_FIREBASE_STORAGE_BUCKET!,
+  messagingSenderId: process.env.CN_FIREBASE_MESSAGING_SENDER_ID!,
 };
