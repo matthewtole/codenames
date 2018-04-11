028865aa2219af00565ab6c94b56a67a221a3f7c
Matthew Tole <matthewtole@gmail.com>
2018-04-11 10:24:49 -0700

    Load Firebase configuration from environment variables

--- src/config.ts
+++ src/config.ts
@@ -1,8 +1,25 @@
-export const firebase = {
-  apiKey: 'AIzaSyCtUUcrXPb-LnL94AN2Y0GBTOgRE4EVtzo',
-  authDomain: 'codenames-7d699.firebaseapp.com',
-  databaseURL: 'https://codenames-7d699.firebaseio.com',
-  projectId: 'codenames-7d699',
-  storageBucket: 'codenames-7d699.appspot.com',
-  messagingSenderId: '1062407981318',
+import * as dotenv from 'dotenv';
+import * as assert from 'assert';
+import { FirebaseConfig } from './lib/firebase';
+
+dotenv.load();
+
+[
+  'FIREBASE_API_KEY',
+  'FIREBASE_AUTH_DOMAIN',
+  'FIREBASE_DATABASE_URL',
+  'FIREBASE_PROJECT_ID',
+  'FIREBASE_STORAGE_BUCKET',
+  'FIREBASE_MESSAGING_SENDER_ID',
+].forEach(key =>
+  assert(process.env[key], `Missing required environment variable: "${key}"`)
+);
+
+export const firebase: FirebaseConfig = {
+  apiKey: process.env.FIREBASE_API_KEY!,
+  authDomain: process.env.FIREBASE_AUTH_DOMAIN!,
+  databaseURL: process.env.FIREBASE_DATABASE_URL!,
+  projectId: process.env.FIREBASE_PROJECT_ID!,
+  storageBucket: process.env.FIREBASE_STORAGE_BUCKET!,
+  messagingSenderId: process.env.FIREBASE_MESSAGING_SENDER_ID!,
 };
