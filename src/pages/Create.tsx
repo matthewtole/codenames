7437df2830ef57d82abaef4ad4032d656267a0ac
Matthew Tole <matthewtole@gmail.com>
2018-04-12 19:52:41 -0700

    :sparkles: Add Instructions box to the Create Game page
    Fixes #21

--- src/pages/Create.tsx
+++ src/pages/Create.tsx
@@ -1,13 +1,58 @@
 import * as React from 'react';
 import { SetupContainer } from '../containers/Setup';
 import { Navbar } from '../components/Navbar';
+import { Link } from 'react-router-dom';
+import { DictionaryNames } from '../lib/dictionary';
 
 export class CreatePage extends React.Component<{}, {}> {
   render() {
     return (
       <div className="has-navbar-fixed-top">
         <Navbar page="create" />
-        <SetupContainer />
+        <div>
+          <section className="hero is-primary">
+            <div className="hero-body">
+              <div className="container has-text-centered">
+                <h1 className="title">Create Game</h1>
+              </div>
+            </div>
+          </section>
+          <section className="section">
+            <div className="container is-fluid">
+              <div className="columns is-mobile is-multiline">
+                <div className="column is-one-third-desktop is-one-half-tablet is-full-mobile">
+                  <div className="box">
+                    <h3 className="title is-size-4">Instructions</h3>
+                    <p className="text">
+                      If you haven't already, check out the{' '}
+                      <Link to="/help#basics">Codenames Basics</Link> guide.
+                    </p>
+                    <p className="text">
+                      Here is where you create a Codenames game. You should be
+                      doing this on whatever device the Spymasters will be
+                      using, usually a tablet or phone.
+                    </p>
+                    <p className="text">
+                      Choose between the {DictionaryNames.length} dictionaries,
+                      and then select the additional ruleset if you want to play
+                      the drinking or strip versions of Codenames.{' '}
+                      <em>
+                        (Don't worry, you can always change your mind later on!)
+                      </em>
+                    </p>
+                    <p className="text has-text-weight-semibold">
+                      Need more help? Don't fret, check out the{' '}
+                      <Link to="/help">Help page</Link>.
+                    </p>
+                  </div>
+                </div>
+                <div className="column is-half-desktop is-half-tablet is-full-mobile is-one-third-widescreen">
+                  <SetupContainer />
+                </div>
+              </div>
+            </div>
+          </section>
+        </div>
       </div>
     );
   }
--- src/pages/Help.tsx
+++ src/pages/Help.tsx
@@ -6,7 +6,46 @@ export class HelpPage extends React.Component<{}, {}> {
     return (
       <div className="has-navbar-fixed-top">
         <Navbar page="help" />
-        <h1>Help!</h1>
+        <div>
+          <section className="hero is-primary">
+            <div className="hero-body">
+              <div className="container has-text-centered">
+                <h1 className="title">Codenames Help</h1>
+              </div>
+            </div>
+          </section>
+          <section className="section">
+            <div className="container is-fluid" />
+            <div className="columns">
+              <div className="column is-one-quarter">
+                <div className="box">
+                  <aside className="menu">
+                    {/* <p className="menu-label">General</p>
+                    <ul className="menu-list">
+                      <li>
+                        <a>Dashboard</a>
+                      </li>
+                      <li>
+                        <a>Customers</a>
+                      </li>
+                    </ul> */}
+                    {/* <p className="menu-label">Administration</p> */}
+                    <ul className="menu-list">
+                      <li>
+                        <a href="#contact">Contact</a>
+                      </li>
+                    </ul>
+                  </aside>
+                </div>
+              </div>
+              <div className="column">
+                <h2 className="is-size-3" id="contact">
+                  Contact
+                </h2>
+              </div>
+            </div>
+          </section>
+        </div>
       </div>
     );
   }
